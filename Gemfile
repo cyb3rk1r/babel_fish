d8f4776259d2d72f35388699e5a6f20264544b88
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2018-08-27 23:05:35 +0300

    logging

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby "2.5.1"
 
 gem 'telegram-bot-ruby'
 gem 'easy_translate'
+gem 'semantic_logger'
--- main.rb
+++ main.rb
@@ -1,8 +1,30 @@
 require 'telegram/bot'
 require 'easy_translate'
+require 'semantic_logger'
 
 telegram_token = ENV.fetch('TELEGRAM_TOKEN', nil)
 google_api_token = ENV.fetch( 'GOOGLE_API_TOKEN', nil)
+EasyTranslate.api_key = google_api_token
+SemanticLogger.default_level = :trace
+SemanticLogger.add_appender(io: $stdout, formatter: :color)
+
+logger = SemanticLogger['BabelFish']
+
+babel_fish_logo = <<EOF
+                                                    _
+                  __....---------------.       _.-'' |
+               ,:'_ \__      '._`-.,--- `.    / _.-' |
+     _       ,'',-.`.  |_____   `-:_`-.,- \  / / _.- |
+    | `.   ,' : `-' ;_,'---. `--..__`-:._`-`' /,'__. :
+    |:  `.' o-'`---'  |    |     .--`---<----<:-..__ /
+    |::--._.      __.-'  _ |.--.-'---.   )-,. \\`. .  \\
+    |:  ,.  `'`.,'  , , / |:| _|    ,' ,`-/  \ \\ `. :
+    |_,'  `-.    _.',' (_ |:| _| _,','`- /    \ \`.  |
+             `-.__  [|_| ||:|__,','`--- /      \ \ ` |
+                  `-..._______.:..-----' SSt    \_`. |
+                                                  `-.|
+EOF
+logger.info(babel_fish_logo)
 
 Telegram::Bot::Client.run(telegram_token) do |bot|
   bot.listen do |message|
@@ -12,14 +34,18 @@ Telegram::Bot::Client.run(telegram_token) do |bot|
       next
     end
 
+    logger.info(payload: { message_text: message.text,
+                           chat_id: message.chat.id })
+
     begin
       case message.text
       when '/start'
+        # create vocabulary for that chat
         bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
       when '/stop'
         bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
       else
-        translate =  EasyTranslate.translate(message.text, :to => :ru, :key => google_api_token)
+        translate =  EasyTranslate.translate(message.text, :to => :ru)
         bot.api.send_message(chat_id: message.chat.id, text: translate)
       end
     rescue => e
